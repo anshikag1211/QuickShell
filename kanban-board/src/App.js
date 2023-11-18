da94b8326e409097c8837731c74dc284417d99ae
bhanu-55 <bhanu.sharma@fiftyfivetech.io>
2023-11-18 23:22:13 +0530

    adding new code

---
+++ kanban-board/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import YourComponent from "./component/YourComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <YourComponent/>
+    </div>
+  );
+}
+
+export default App;
